a627d1062251cc43f75c0d7e3289f60c621cd4c3
grahagandangr <graha@10xers.co>
2025-02-02 16:26:29 +0700

    feat(setup): setup expo router and nativewind

---
+++ app/index.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function News() {
+  return (
+    <View className="flex-1 bg-white">
+      <Text className="text-red-300">News</Text>
+    </View>
+  );
+}
